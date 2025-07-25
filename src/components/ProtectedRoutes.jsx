3af1aa071f2d18f5087b44660fd22e83e3758f9d
sardorbek-3226 <ibrohimovs@gmail.com>
2025-07-25 13:47:47 +0300

    second commit

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoutes({ user, children }) {
-  if (!user) {
+  if (user) {
     return children;
   } else {
     return <Navigate to="/login" />;
